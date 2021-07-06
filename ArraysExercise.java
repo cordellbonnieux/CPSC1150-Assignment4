e242d3e3d3ece9323cf78007a18c62c4eb4cef04
Cordell <cordellbonnieux@gmail.com>
2021-07-05 20:58:09 -0700

    Reworked the structure, makes more sense now

--- ArraysExercise.java
+++ ArraysExercise.java
@@ -1,43 +1,53 @@
-import java.util.Scanner;
-public static class ArraysExercise {
+import java.util.*;
+public class ArraysExercise {
     public static void main(String[] args) {
 
-        //
+        // create scanner obj
         Scanner input = new Scanner(System.in);
 
         // call question 1
         int[] arr = getArray(input);
+        System.out.println("ok!");
 
-        System.out.println(arr);
-
+        
 
     }
-    /**
-     * Question 1
+    /** 
+     * Question 1 - getArray
      * @param input
      * @return
      */
     public static int[] getArray(Scanner input) {
 
-        // prompt user
-        System.out.print("Please enter an array size(positive integer): ");
-
-        // read integer into size
-        int size = input.nextInt();
-
-        // check if size is valid
-        if (!(size == (int)size) || size < 1)
-            return getArray(input);
+        //
+        int size = getSize(input);
         
         // create an array that counts up to size
-        int[] sizeArray;
+        int[] array = new int[size];
 
         //fill up the array
         for (int i = 1; i <= size; i++) {
 
-            sizeArray[(i-1)] += i;
+            int pos = i - 1;
+
+            array[pos] = i;
         }
 
-        return sizeArray;
+        return array;
+    }
+
+    public static int getSize(Scanner input) {
+
+            // prompt user
+            System.out.print("Please enter an array size(positive integer): ");
+
+            // read integer into size
+            int size = input.nextInt();
+        
+            // check if size is valid
+            if (!(size == (int)size) || size < 1)
+                return getSize(input);
+
+            return size;
     }
 }
