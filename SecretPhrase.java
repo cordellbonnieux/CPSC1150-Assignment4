90cf8c2df8264a8d8418255508412f7bb7e9f54d
Cordell <cordellbonnieux@gmail.com>
2021-07-12 16:07:44 -0700

    The games logic is now complete and working.

--- SecretPhrase.java
+++ SecretPhrase.java
@@ -19,7 +19,11 @@ public class SecretPhrase {
 
         while (!complete) {
 
-            if (secretPhrase != phrase) {
+            if (secretPhrase.equals(phrase)) {
+
+                complete = true;
+
+            } else {
                 
                 // read a guess from the user
                 char userGuess = guess(phrase, counter);
@@ -30,8 +34,7 @@ public class SecretPhrase {
                 // count the rounds
                 counter++;  
 
-            } else
-                complete = true;
+            }
         }
         System.out.println("the secret phrase is: " + secretPhrase); // testing
         System.out.println("the scrambled phrase is: " + phrase); // testing
@@ -48,7 +51,7 @@ public class SecretPhrase {
     public static char guess(String phrase, int counter) {
 
         // prompt user
-        System.out.println("Round #" + counter + ": Guess a letter to complete this sentence: \n" + phrase);
+        System.out.print("Round #" + counter + ": Guess a letter to complete this sentence: '" + phrase + "' :");
 
         // scanner obj
         Scanner scan = new Scanner(System.in);
@@ -122,30 +125,48 @@ public class SecretPhrase {
     }
 
     public static int[] scrambleIndex(String phrase, int[] spaceArr) {
+    System.out.println("ENTERED scrambleIndex ----------"); // testing
 
         // create a scramble index list
-        int[] scrambleIndex = new int[((phrase.length() / 2))];
+        int[] scrambleIndex = new int[(((phrase.length() - spaceArr.length) / 2))];
+
+    System.out.println("scrambleIndex array length = " + scrambleIndex.length); // testing
 
         // choose scramble indexes
         int i = 0;
         while (i < scrambleIndex.length) {
+        
+        System.out.println("ENTERED while loop " + i); // testing
 
+            // select a random index
             int pos = (int)(Math.random() * (phrase.length() - 0)) + 0;
+
+        System.out.println("random num pos = " + pos); // testing
 
             // check if the pos has any duplicate letters
             int duplicateCounter = 0;
             for (int y = 0; y < phrase.length(); y++) {
-                if (Character.toLowerCase(phrase.charAt(y)) == Character.toLowerCase(phrase.charAt(pos))) {
+                if (phrase.charAt(y) == ' ' || phrase.charAt(y) == ',' || phrase.charAt(y) == '.' || phrase.charAt(y) == '!')
+                    continue;
+                else if (phrase.charAt(y) == phrase.charAt(pos) && y != pos)
                     duplicateCounter++;
-                }
             }
+
+        System.out.println("letter @ pos = " + phrase.charAt(pos) + ", number of duplicates = " + duplicateCounter); // testing
+
+            // if the number of duplicates exceed the free space left in the array, break.
+            if ((scrambleIndex.length - (i + 1)) <= duplicateCounter) break;
+
             // find each duplicate and store in an array
             int[] duplicates = new int[duplicateCounter];
             int d = 0;
             for (int y = 0; y < phrase.length(); y++) {
-                if (Character.toLowerCase(phrase.charAt(y)) == Character.toLowerCase(phrase.charAt(pos))) {
+                if (phrase.charAt(y) == ' ' || phrase.charAt(y) == ',' || phrase.charAt(y) == '.' || phrase.charAt(y) == '!')
+                    continue;
+                else if (phrase.charAt(y) == phrase.charAt(pos) && y != pos) {
                     duplicates[d] = y;
                     d++;
+                System.out.println("position " + y + " of phrase stored in duplicates array (representing '" + phrase.charAt(y) + "')"); // testing
                 }
             }
 
@@ -163,18 +184,18 @@ public class SecretPhrase {
             }
 
             // add to scramble index list
-            if (valid) {
+            if (duplicateCounter > 0 && valid && (duplicates.length + 1) < (scrambleIndex.length - i)) {
                 scrambleIndex[i] = pos;
                 i++;
-
-            } else if (duplicateCounter > 0 && valid) {
                 for (int y = 0; y < duplicates.length; y++) {
-
-                    if (i >= scrambleIndex.length) break;
-
                     scrambleIndex[i] = duplicates[y];
                     i++;
+                System.out.println("index " + y + " added to final scramble list"); // testing
                 }
+            } else if (valid) {
+                scrambleIndex[i] = pos;
+                i++;
+                System.out.println("index " + pos + " added to final scramble list"); // testing
             }
         }
 
@@ -183,16 +204,16 @@ public class SecretPhrase {
 
     public static String checkPhrase(String phrase, String secretPhrase, char userGuess) {
 
-        String copy = phrase;
+        String copy = "";
 
         for (int i = 0; i < phrase.length(); i++) {
-            if (copy.charAt(i) == '*' && secretPhrase.charAt(i) == userGuess)
-                phrase += userGuess;
+            if (phrase.charAt(i) == '*' && secretPhrase.charAt(i) == userGuess)
+                copy += userGuess;
             else
-                phrase += copy.charAt(i);
+                copy += phrase.charAt(i);
         }
 
-        return phrase;
+        return copy;
     }
 
 }
