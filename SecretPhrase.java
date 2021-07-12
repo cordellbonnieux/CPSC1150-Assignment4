bb264b4b0d4cffd2f05decc1b74cdbbcfbe3fa2c
Cordell <cordellbonnieux@gmail.com>
2021-07-12 13:57:59 -0700

    The scrambler works now after a lot of trial and error.

--- SecretPhrase.java
+++ SecretPhrase.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 public class SecretPhrase {
     public static void main(String[] args) {
 
@@ -7,7 +8,7 @@ public class SecretPhrase {
         System.out.println("the secret phrase is: " + secretPhrase); // testing
 
         // make a hidden phrase version
-        String phrase = scramblePhrase(secretPhrase);
+        String phrase = scramblePhrase(secretPhrase, scrambleIndex(secretPhrase, spaces(secretPhrase)));
         System.out.println("the scrambled phrase is: " + phrase); // testing
 
         // triggers when phrase == secret phrase
@@ -56,14 +57,14 @@ public class SecretPhrase {
         char guess = scan.next().charAt(0);
 
         // make sure guess is not a number
-        if (guess == (int)guess || guess == (double)guess){
-
+        Boolean isDigit = Character.isDigit(guess);
+        if (isDigit){
             System.out.println("Your guess was a number, please enter a letter.");
             return guess(phrase, counter);
         }
 
         // make sure the guess is not already revealed
-        for (int i = 0; i < phrase.length(); i++) {
+        for (int i = 0; i < phrase.length() - 1; i++) {
             if (phrase.charAt(i) == guess) {
 
                 System.out.println("That letter is already revealed, pick a new letter.");
@@ -73,30 +74,111 @@ public class SecretPhrase {
 
         return guess;
     }
-    public static String scramblePhrase(String phrase) {
+    public static String scramblePhrase(String phrase, int[] scrambleIndex) {
 
-        // get 2/3 the the length of string
-        int asteriks = (phrase.length() / 3) * 2;
-
+        //hold the scrambled phrase
         String copy = "";
 
-        while (asteriks > 0) {
+        // scramble the phrase
+        for (int w = 0; w < phrase.length(); w++) {
+
+            // assign asteriks'
+            for (int v = 0; v < scrambleIndex.length; v++) {
+                if (scrambleIndex[v] == w) {
+                    copy += '*';
+                    break;
+                }      
+            }
+
+            if (copy.length() <= w)
+                    copy += phrase.charAt(w);
+        }
+
+        return copy;
+    }
+
+    public static int[] spaces(String phrase) {
+
+        // count the number of spaces and punctuation marks in the phrase
+        int spaces = 0;
+        for (int i = 0; i < phrase.length(); i++) {
+        if (phrase.charAt(i) == ' ' || phrase.charAt(i) == ',' || phrase.charAt(i) == '.' || phrase.charAt(i) == '!')
+            spaces++;
+        }
+        
+        // create an array which indexes the spaces and punctuation
+        int[] spaceArr = new int[spaces];
+        
+        // add the space and punctuation indexes to the array
+        int x = 0;
+        for (int i = 0; i < phrase.length(); i++) {
+            if (phrase.charAt(i) == ' ' || phrase.charAt(i) == ',' || phrase.charAt(i) == '.' || phrase.charAt(i) == '!') {
+                spaceArr[x] = i;
+                x++;
+            }
+        }
+
+        return spaceArr;
+    }
+
+    public static int[] scrambleIndex(String phrase, int[] spaceArr) {
+
+        // create a scramble index list
+        int[] scrambleIndex = new int[((phrase.length() / 2))];
+
+        // choose scramble indexes
+        int i = 0;
+        while (i < scrambleIndex.length) {
 
             int pos = (int)(Math.random() * (phrase.length() - 0)) + 0;
 
-            for (int i = 0; i < phrase.length(); i++) {
-
-                if (i != pos)
-                    copy += phrase.charAt(i);
-
-                else {
-                    copy += '*';
-                    asteriks--;
+            // check if the pos has any duplicate letters
+            int duplicateCounter = 0;
+            for (int y = 0; y < phrase.length(); y++) {
+                if (Character.toLowerCase(phrase.charAt(y)) == Character.toLowerCase(phrase.charAt(pos))) {
+                    duplicateCounter++;
                 }
+            }
+            // find each duplicate and store in an array
+            int[] duplicates = new int[duplicateCounter];
+            int d = 0;
+            for (int y = 0; y < phrase.length(); y++) {
+                if (Character.toLowerCase(phrase.charAt(y)) == Character.toLowerCase(phrase.charAt(pos))) {
+                    duplicates[d] = y;
+                    d++;
+                }
+            }
 
+            // check if pos already exists
+            Boolean valid = true;
+            for (int y = 0; y < scrambleIndex.length; y++) {
+                if (scrambleIndex[y] == pos)
+                    valid = false;
+            }
+
+            // check if there is a space at that pos
+            for (int y = 0; y < spaceArr.length; y++){
+                if (spaceArr[y] == pos)
+                    valid = false;
+            }
+
+            // add to scramble index list
+            if (valid) {
+                scrambleIndex[i] = pos;
+                i++;
+
+            } else if (duplicateCounter > 0 && valid) {
+                for (int y = 0; y < duplicates.length; y++) {
+
+                    if (i >= scrambleIndex.length) break;
+
+                    scrambleIndex[i] = duplicates[y];
+                    i++;
+                }
             }
         }
-        return copy;
+
+        return scrambleIndex;
     }
 
     public static String checkPhrase(String phrase, String secretPhrase, char userGuess) {
